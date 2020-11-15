eb7d1c45ab4b64d0c5701283b534b5a7774c5f0a
Matthias Gliwka <matthias@gliwka.eu>
2020-11-16 00:28:49 +0100

    Check file hash of dependencies to verify authenticity

--- build.sh
+++ build.sh
@@ -4,19 +4,26 @@
 set -xeu
 set -o pipefail
 
+HYPERSCAN=5.3.0
+
 THREADS=$(nproc --all)
 
 mkdir -p target/lib
 mkdir -p target/include/hs
 cd target
 
-git clone -b v5.3.0 --depth 1 https://github.com/intel/hyperscan.git
+curl -LOJ https://github.com/intel/hyperscan/archive/v$HYPERSCAN.tar.gz
+echo "9b50e24e6fd1e357165063580c631a828157d361f2f27975c5031fc00594825b hyperscan-$HYPERSCAN.tar.gz" | sha256sum -c
+tar -zxf hyperscan-$HYPERSCAN.tar.gz
 
-curl -LOJ https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
-tar -zxf boost_1_65_1.tar.gz
-mv boost_1_65_1/boost hyperscan/include/boost
+curl -LOJ https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.gz
+echo "afff36d392885120bcac079148c177d1f6f7730ec3d47233aa51b0afa4db94a5 boost_1_74_0.tar.gz" | sha256sum -c
+tar -zxf boost_1_74_0.tar.gz
+mv boost_1_74_0/boost hyperscan-$HYPERSCAN/include/boost
 
 curl -LOJ https://www.colm.net/files/ragel/ragel-6.10.tar.gz
+echo "5f156edb65d20b856d638dd9ee2dfb43285914d9aa2b6ec779dac0270cd56c3f ragel-6.10.tar.gz" | sha256sum -c
+
 tar -zxf ragel-6.10.tar.gz
 cd ragel-6.10
 ./configure
@@ -24,7 +31,7 @@ make -j $THREADS
 make install
 cd ..
 
-cd hyperscan
+cd hyperscan-$HYPERSCAN
 
 case $OS_ARCH in
 linux-x86_64)
