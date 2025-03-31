61cede6307a3420bbe993e7f4f3f961b001879bf
Matthias Gliwka <matthias@gliwka.eu>
2025-04-01 01:50:29 +0200

    ci: add script to merge artifacts into single deployable

---
+++ merge-artifacts.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+set -euox pipefail
+
+# Make sure all artifacts have the same timestamp overwriting duplicates
+timestamp=$(date +"%Y%m%d.%H%M%S")
+find target/staging-deploy -type f | while read file; do
+    newfile=$(echo "$file" | sed -E "s/[0-9]{8}\.[0-9]{6}/$timestamp/g")
+    if [ "$file" != "$newfile" ]; then
+        mv "$file" "$newfile"
+    fi
+done
+
+# Delete hashsums, they will be recreated
+find target/staging-deploy -type f -name "*.md5" -o -name "*.sha1" -delete
